2d39f75050e5fa19b692946ce8244e08c6c50f90
Eyal Dechter <eyaldechter@gmail.com>
2016-10-25 14:16:15 -0400

    Typo

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(with).
 title('Context managers for SWI Prolog').
-version('v0.0.2')g.
+version('v0.0.2').
 author('Eyal Dechter', 'eyaldechter@gmail.com').
 maintainer('Eyal Dechter', 'eyaldechter@gmail.com').
 packager('Eyal Dechter', 'eyaldechter@gmail.com').
