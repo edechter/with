09717ee6208fa3ba7ae35a81109006f679a22d95
Eyal Dechter <eyaldechter@gmail.com>
2016-10-25 14:15:15 -0400

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(with).
 title('Context managers for SWI Prolog').
-version('v0.0.1').
+version('v0.0.2')g.
 author('Eyal Dechter', 'eyaldechter@gmail.com').
 maintainer('Eyal Dechter', 'eyaldechter@gmail.com').
 packager('Eyal Dechter', 'eyaldechter@gmail.com').
