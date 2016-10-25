940ab9dda7a259e298191dba57e8a58186475454
Eyal Dechter <eyaldechter@gmail.com>
2016-10-25 14:11:24 -0400

    Update pack download url

--- pack.pl
+++ pack.pl
@@ -4,4 +4,4 @@ version('0.0.1').
 author('Eyal Dechter', 'eyaldechter@gmail.com').
 maintainer('Eyal Dechter', 'eyaldechter@gmail.com').
 packager('Eyal Dechter', 'eyaldechter@gmail.com').
-download('https://github.com/edechter/swip-pack-with/archive/v*.zip').
+download('https://github.com/edechter/swip-pack-with/archive/*.zip').
