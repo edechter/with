3c4fccb37ec05d519820b70ef369bf95b8519a27
Eyal Dechter <eyaldechter@gmail.com>
2016-11-06 16:00:15 -0500

    Fixed download entry of pack.pl

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(with).
 title('Context managers for SWI Prolog').
-version('0.0.5').
+version('0.0.6').
 author('Eyal Dechter', 'eyaldechter@gmail.com').
 maintainer('Eyal Dechter', 'eyaldechter@gmail.com').
 packager('Eyal Dechter', 'eyaldechter@gmail.com').
-download('https://github.com/edechter/with/archive/*.zip').
+download('https://github.com/edechter/with/releases/*.zip').
